40cc2354c4acde14db434309f7dbcfbedf975391
Nil Késede <nilkesede@gmail.com>
2020-06-02 21:10:44 -0300

    Add AC button action

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -2,7 +2,14 @@ class Memory {
   String _value = '0';
 
   void applyCommand(String command) {
+    if (command == 'AC')
+      return this._allClear();
+
     this._value += command;
+  }
+
+  _allClear() {
+    this._value = '0';
   }
 
   String get value {
