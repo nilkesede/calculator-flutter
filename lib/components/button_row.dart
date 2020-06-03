dabc8140ab41df0f3670ca0a8914ba1117804307
Nil Késede <nilkesede@gmail.com>
2020-06-02 21:17:42 -0300

    Add btw button spaces

--- lib/components/button_row.dart
+++ lib/components/button_row.dart
@@ -12,7 +12,10 @@ class ButtonRow extends StatelessWidget {
       flex: 1,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: this.buttons,
+        children: this.buttons.fold(<Widget>[], (list, button) {
+          list.isEmpty ? list.add(button) : list.addAll([SizedBox(width: 1), button]);
+          return list;
+        }),
       ),
     );
   }
--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -18,24 +18,28 @@ class Keyboard extends StatelessWidget {
             Button(text: '%', color: Button.DARK, cb: this.cb),
             Button.operation(text: '/', cb: this.cb),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
             Button(text: '7', cb: this.cb),
             Button(text: '8', cb: this.cb),
             Button(text: '9', cb: this.cb),
             Button.operation(text: 'x', cb: this.cb),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
             Button(text: '4', cb: this.cb),
             Button(text: '5', cb: this.cb),
             Button(text: '6', cb: this.cb),
             Button.operation(text: '-', cb: this.cb)
           ]),
+          SizedBox(height: 1),
           ButtonRow([
             Button(text: '1', cb: this.cb),
             Button(text: '2', cb: this.cb),
             Button(text: '3', cb: this.cb),
             Button.operation(text: '+', cb: this.cb)
           ]),
+          SizedBox(height: 1),
           ButtonRow([
             Button.big(text: '0', cb: this.cb),
             Button(text: ',', cb: this.cb),
