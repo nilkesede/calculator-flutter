0ad37fa2b5dd70be806892a1de999473599f16dc
Nil Késede <nilkesede@gmail.com>
2020-06-02 21:19:18 -0300

    Lock orientation

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../components/display.dart';
 import '../components/keyboard.dart';
 import '../models/memory.dart';
@@ -19,6 +20,10 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+
     return MaterialApp(
       home: Column(
         children: <Widget>[
